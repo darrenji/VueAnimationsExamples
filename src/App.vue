5cd17d1b1a87998347f6837be32421e59feed01e
darren <764190362@qq.com>
2017-10-26 07:22:38 +0800

    JS Animation

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,7 @@
       <div class="row">
           <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
            
-            <h3>Hover us</h3>
-            <main>
-                <app-child>
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo2.jpg" alt="">
-                </app-child>
-                <app-child>
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo13.jpg" alt="">
-                </app-child>
-                <app-child>
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo14.jpeg" alt="">
-                </app-child>
-                <app-child>
-                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo5.jpg" alt="">
-                </app-child>
-            </main>
+
             
           </div>
       </div>
@@ -30,12 +16,18 @@
     export default {
         data(){
             return {
-                isShowing: false
+                message: 'This is a good place to type things.',
+                load: false
             }
         },
         components: {
             appChild: Child
         },
+        methods: {
+            beforeEnter(el){
+                
+            }
+        }
     }
 </script>
 
