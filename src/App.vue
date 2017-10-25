7d2f0f089df8a4e08330791404a198913f5a526e
darren <764190362@qq.com>
2017-10-25 21:51:44 +0800

    真正理解了enter和leave的几个状态，并加深了对v-bind:class的理解

--- src/App.vue
+++ src/App.vue
@@ -2,109 +2,77 @@
   <div class="container">
       <div class="row">
           <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-              <h1>Animations</h1>
-              <select v-mode="alertAnimation" class="form-control">
-                  <option value="fade">Fade</option>
-                  <option value="slide">Slide</option>
-              </select>
+           
               <hr>
-              <button class="btn btn-primary" @click="show = !show">Show Alert</button>
-              <br>
-              <br>
-              <transition :name="alertAnimation">
-<!--                 一个transition内只能包含一个元素-->
-                  <div class="alert alert-info" v-show="show">This is some Info</div>
-              </transition>
-              <transition :name="alertAnimation" type="animation" appear>
-<!--                 一个transition内只能包含一个元素-->
-                  <div class="alert alert-info" v-if="show">This is some Info</div>
-              </transition>
-              <transition
-                  enter-active-class="animated bounce"
-                  leave-active-class="animated shake">
-<!--                 一个transition内只能包含一个元素-->
-                  <div class="alert alert-info" v-if="show">This is some Info</div>
-              </transition>
+              <div v-bind:class="[show ? blurClass : '',bkClass]">
+                  <h3>Trigger modal here</h3>
+                  <button @click="toggleShow">
+                      <span v-if="show">Hide Child</span>
+                      <span v-else>Show Child</span>
+                  </button>
+              </div>
               
-              <transition :name="alertAnimation" mode="out-in">
-                  <div class="alert alert-info" v-if="show" key="info">This is some Info bottom</div>
-                  <div class="alert alert-warning" v-else key="warning">This is warning</div>
+              <transition name="fade">
+                 <app-child v-if="show" class="modal1">
+                      <button @click="toggleShow">Close</button>
+                  </app-child>               
               </transition>
+
           </div>
       </div>
   </div>
 </template>
 
 <script>
+    import Child from './Child.vue';
     export default {
         data(){
             return {
-                show: true,
-                alertAnimation: 'fade'
+                show: false,
+                alertAnimation: 'fade',
+                bkClass: 'bk',
+                blurClass: 'blur'
             }
+        },
+        methods: {
+            toggleShow(){
+                this.show = !this.show;
+            }
+        },
+        components: {
+            appChild: Child
         }
     }
 </script>
 
 <style>
-    .fade-enter {
-        opacity: 0;
-    }
     
-    .fade-enter-active {
-        transition: opacity 1s;
-    }
-    
-    .fade-leave {
-        
-    }
-    
-    .fade-leave-active {
-        transition: opacity 1s;
-        opacity: 0;
-    }
-    
-    .slide-enter {
-        
 /*
-        下面设置过了就不需要设置了
-        transform: translateY(20px);
+    enter, enter-active, enter-to
+    leave, leave-active, leave-to
+    如果我们想针对transform做transition效果的话，那enter-active使用ease-out, leave-active使用ease-in
 */
+    
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 0.25s ease-out;
+    }
+    
+/*    
+    enter和leave-to是开始和离开的节点
+    enter的时候是绑定，leave-to的时候是解绑
+    为什么不在enter-to上设置opacity:1呢？为什么不在leave上设置opacity:1内？因为每个过程节点上的opactiy的默认值就是1
+*/
+    
+    .fade-enter, .fade-leave-to {
         opacity: 0;
     }
     
-    .slide-enter-active {
-        animation: slide-in 1s ease-out forwards;
-        transition:  opacity .5s;
+    .bk {
+        transition: all 0.1s ease-out;
     }
     
-    .slide-leave {
-        
-    }
-    
-    .slide-leave-active {
-        animation: slide-out 1s ease-out forwards;
-        transition: opacity 1s;
-        opacity: 0;
-    }
-    
-    @keyframes slide-in {
-        from {
-            transform: translateY(20px);
-        }
-        
-        to {
-            transform: translateY(0);
-        }
-    }
-    
-    @keyframes slide-out {
-        from {
-            transform: translateY(0);
-        }
-        
-        to {
-            transform: translateY(20px);
-        }
+    .blur {
+        filter: blur(2px);
+        opacity: 0.4;
     }
 </style>
---
+++ src/Child.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>I am child</h2>
+        <slot></slot>
+    </div>
+</template>
+
+
+<style>
+    .modal1 {
+        background: cyan;
+        color: black;
+        padding: 20px;
+        width: 200px;
+    }
+</style>
