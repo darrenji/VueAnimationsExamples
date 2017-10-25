ff42be6c207c4cd716c36521cfca62fce2c46568
darren <764190362@qq.com>
2017-10-25 22:28:27 +0800

    尺寸变化的动画效果

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,25 @@
       <div class="row">
           <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
            
-              <hr>
-              <div v-bind:class="[show ? blurClass : '',bkClass]">
-                  <h3>Trigger modal here</h3>
-                  <button @click="toggleShow">
-                      <span v-if="show">Hide Child</span>
-                      <span v-else>Show Child</span>
-                  </button>
-              </div>
-              
-              <transition name="fade">
-                 <app-child v-if="show" class="modal1">
-                      <button @click="toggleShow">Close</button>
-                  </app-child>               
-              </transition>
-
+            <button @click="show = !show">Toggle Visibility</button><br>
+            
+            <transition name="bounce">
+                <h1 v-show="show">{{text}}</h1>
+            </transition>
+            
           </div>
       </div>
   </div>
 </template>
 
 <script>
-    import Child from './Child.vue';
+
     export default {
         data(){
             return {
-                show: false,
-                alertAnimation: 'fade',
-                bkClass: 'bk',
-                blurClass: 'blur'
+                text: 'Hello World',
+                show: false
             }
-        },
-        methods: {
-            toggleShow(){
-                this.show = !this.show;
-            }
-        },
-        components: {
-            appChild: Child
         }
     }
 </script>
@@ -53,8 +34,8 @@
     如果我们想针对transform做transition效果的话，那enter-active使用ease-out, leave-active使用ease-in
 */
     
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity 0.25s ease-out;
+    .bounce-enter-active, .bounce-leave-active {
+        transition: all .5s;
     }
     
 /*    
@@ -63,16 +44,11 @@
     为什么不在enter-to上设置opacity:1呢？为什么不在leave上设置opacity:1内？因为每个过程节点上的opactiy的默认值就是1
 */
     
-    .fade-enter, .fade-leave-to {
-        opacity: 0;
+    .bounce-enter, .bounce-leave-to {
+        transform: scale(0);
     }
     
-    .bk {
-        transition: all 0.1s ease-out;
-    }
-    
-    .blur {
-        filter: blur(2px);
-        opacity: 0.4;
+    h1 {
+        display: inline-block;
     }
 </style>
