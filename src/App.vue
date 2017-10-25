d2282f602c1f661c69a12a779dcfa64ef8f2fc98
darren <764190362@qq.com>
2017-10-25 23:40:06 +0800

    使用mode在设置进入退出的方式

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,21 @@
       <div class="row">
           <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
            
-          <h3>Bounce the Ball</h3>
-           <button @click="toggleShow">
-               <span v-if="isShowing">开始</span>
-               <span v-else>让球滚动</span>
-           </button>
-           
-           <transition 
-              name="ballmove"
-              enter-active-class="bouncein"
-              leave-active-class="rollout">
-               <div v-if="isShowing">
-                   <app-child class="child"></app-child>
-               </div>
-           </transition>
+            <h3>Hover us</h3>
+            <main>
+                <app-child>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo2.jpg" alt="">
+                </app-child>
+                <app-child>
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo13.jpg" alt="">
+                </app-child>
+                <app-child>
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo14.jpeg" alt="">
+                </app-child>
+                <app-child>
+                    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo5.jpg" alt="">
+                </app-child>
+            </main>
             
           </div>
       </div>
@@ -35,73 +36,22 @@
         components: {
             appChild: Child
         },
-        methods: {
-            toggleShow(){
-                this.isShowing = !this.isShowing;
-            }
-        }
     }
 </script>
 
 <style>
-    
-    @keyframes bouncein {
-        1% {
-            transform: translate3d(0, -400px, 0);
-        }
-        20%, 40%, 60%, 80%, 95%, 99%, 100% {
-            transform: translate3d(0, 0, 0);
-        }
-        30% {
-            transform: translate3d(0, -80px, 0);
-        }
-        50% {
-            transform: translate3d(0, -40px, 0);
-        }
-        70% {
-            transform: translate3d(0, -30px, 0);
-        }
-        90% {
-            transform: translate3d(0, -15px, 0);
-        }
-        97% {
-            transform: translate3d(0, -10px, 0);
-        }
+    img {
+        width: 100%;
+        transform-origin: 50% 50%;
+        cursor: pointer;
+        transform: scaleY(1) translateZ(0);
+        margin: 5px;
     }
     
-    .bouncein {
-        animation: bouncein 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+    main {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
     }
-
-    .ballmove-enter {
-        transform: translate3d(0, -400px, 0);
-    }
-    
-    @keyframes rollout {
-        0% {
-            transform: translate3d(0, 300px, 0);
-        }
-        100% {
-            transform: translate3d(1000px, 300px, 0);
-        }
-    }
-    
-    @keyframes ballroll {
-        0% {
-            transform: rotate(0);
-        }
-        100% {
-            transform: rotate(1000deg);
-        }
-    }
-    
-    .rollout {
-        width: 60px;
-        height: 60px;
-        animation: rollout 2s cubic-bezier(0.55, 0.085, 0.68, 0.53) both;
-    }
-    
-    .rollout div {
-        animation: ballroll 2s cubic-bezier(0.55, 0.085, 0.68, 0.53) both;
-    }
+   
 </style>
--- src/Child.vue
+++ src/Child.vue
@@ -1,15 +1,57 @@
 <template>
-    <div class="ball">
+    <div class="img-contain">
+        <div class="overlay">
+            <p class="overlay-text">I dont like this one</p>
+            <button @click="toggleShow">Replace</button>
+        </div>
+        
+        <transition name="flip" mode="out-in">
+            <slot v-if="!isShowing"></slot>
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/cartoonvideo14.jpeg" v-else alt="">
+        </transition>
     </div>
 </template>
 
+<script>
+    export default {
+        data(){
+            return {
+                
+            }
+        }, 
+        props: ['isShowing'],
+        methods: {
+            toggleShow(){
+                this.isShowing = !this.isShowing;
+            }
+        }
+    }
+</script>
+
 
 <style>
-    .ball {
-        width: 60px;
-        height: 60px;
-        transform-origin: 50% 50%;
-        transform: translate3d(0, 300px 0) rotate(0);
-        background: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/soccerball.svg");
+    .img-contain {
+        width: 250px;
+        height: 160px;
+        overflow: hidden;
     }
+    
+    .img-contain .overlay {
+        position: absolute;
+        z-index: 1000;
+        display: block;
+        width: 245px;
+        height: 155px;
+        margin: 5px;
+        opacity: 0;
+        overflow: hidden;
+        transition: 0.3s opacity ease-in;
+    }
+    
+    .img-contain:hover .overlay {
+        opacity: 1;
+        background: hsla(50, 0%, 0%, 0.6);
+        transition: 0.3s opacity ease-out;
+    }
+
 </style>
